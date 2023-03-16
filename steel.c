8f58f2b25e72bc128f7a223dd179318d2c5e8e68
BrandonHalmai <76132565+BrandonHalmai@users.noreply.github.com>
2023-03-16 23:54:19 +1100

    Add files via upload

---
+++ steel.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    double max;
+    double pieces;
+    double size;
+    double total;
+    double amount;
+    double exact;
+    printf("What is the total lenght of the rebar? Default is 9m.\n>");
+    scanf("%lf", &max);
+    printf("How many pieces?\n>");
+    scanf("%lf", &pieces);
+    printf("What size?\n>");
+    scanf("%lf", &size);
+    total = pieces * size;
+    amount = total/max;
+    exact = ceil(amount);
+    printf("Total steel needed is %lf\n", total);
+    printf("Total amount of rebar neeed is %lf\n", amount);
+    printf("Rounded to %lf\n", exact);
+}
