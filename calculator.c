a493e489a304350a875734cc6b57d58bfb3d0e9d
Brandon Halmai <brandonhalmai3234@gmail.com>
2025-05-16 23:03:43 +1000

    Working selector with failsafe

--- calculator.c
+++ calculator.c
@@ -4,23 +4,43 @@ char confirmation;
 
 
 int addition() {
-	printf("\nTesting out the addition function");
+	printf("\nTesting out the addition function\n");
 }
 
+int subtraction() {
+	printf("\nTesting out the substraction function\n");
+}
 
-int main() {
-	printf("\nWelcome to the calculator program. Please select the operation you wish to use. [+,-,x,/]\n>");
-	while(1) {
-		scanf("%c", &operation);
-		switch(operation) {
+int multiplication() {
+	printf("\nTesting out the multiplication function\n");
+}
+
+int divison() {
+	printf("\nTesting out the division function\n");
+}
+
+int operationSelector() {
+	scanf(" %c", &operation);
+		switch (operation) {
 			case '+':
 				addition();
-				break;
+				break;	
 			case '-':
-				printf("Subtract");
+				subtraction();
+				break;
+			case 'x':
+				multiplication();
+				break;
+			case '/':
+				divison();
+				break;
 			default:
-				printf("Invalid command. Please try again.");
-		}
+				printf("\nInvalid operation. Please try again.\n>");
+				operationSelector();
+	}
 }
+int main() {
+	printf("\nWelcome to the calculator program. Please select the operation you wish to use. [+,-,x,/]\n>");
+	operationSelector();	
 return 0;
 }
